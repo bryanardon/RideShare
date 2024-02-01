fc63560e130f09dda8c4e236cbab54c1bc3967e7
bryanardon <bryardon01@gmail.com>
2024-01-31 22:14:27 -0800

    Initial Commit

---
+++ RideShare/RideShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideShareApp.swift
+//  RideShare
+//
+//  Created by Bryan Ardon on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct RideShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
